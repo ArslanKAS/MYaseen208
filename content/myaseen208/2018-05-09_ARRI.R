4df979dd93bcabf7c95fdc275717b85dd1c42db3
Muhammad Yaseen <myaseen208@gmail.com>
2018-05-10 21:36:53 +0500

    Ayub Agriculture Research Institute, Faisalabad, Pakistan

--- content/myaseen208/2018-05-09_ARRI.R
+++ content/myaseen208/2018-05-09_ARRI.R
@@ -131,25 +131,24 @@ anova(fm5)
 
 
 ##----anova.df6----
-Consumption <- c(5, 6, 7, 15, 16, 17)
-Gender      <- gl(n = 2, k = 3, length = 2*3, labels = c("Male", "Female"))
-df6         <- data.frame(Consumption, Gender)
+Yield   <- c(5, 6, 7, 15, 16, 17)
+Variety <- gl(n = 2, k = 3, length = 2*3, labels = c("V1", "V2"))
+df6     <- data.frame(Yield, Variety)
 df6
 
 ##----anova.p6----
 library(ggplot2)
 # if (!require("ggplot2")) install.packages("ggplot2")
 p6 <- 
-  ggplot(data = df6, mapping = aes(x = Gender, y = Consumption)) + 
+  ggplot(data = df6, mapping = aes(x = Variety, y = Yield)) + 
   geom_point() +
   geom_boxplot() +
-  scale_y_continuous(labels = scales::dollar) +
-  labs(x = "Gender", y = "Weekly consumption expenditures, $") +
+  labs(x = "Variety", y = "Yield") +
   theme_bw()
 print(p6)
 
 ##----anova.fm6----
-fm6 <- lm(formula = Consumption ~ Gender, data = df6)
+fm6 <- lm(formula = Yield ~ Variety, data = df6)
 summary(fm6)
 
 ##----anova.fm6anova----
@@ -158,26 +157,24 @@ anova(fm6)
 
 
 ##----anova.df7----
-Consumption <- c(5, 7, 15, 17, 17, 19)
-EduGroup    <- gl(n = 3, k = 2, length = 3*2,
-                  labels = c("NoEdu", "SchoolEdu", "CollegeEdu"))
-df7         <- data.frame(Consumption, EduGroup)
+Yield   <- c(5, 7, 15, 17, 17, 19)
+Variety <- gl(n = 3, k = 2, length = 3*2, labels = c("V1", "V2", "V3"))
+df7     <- data.frame(Yield, Variety)
 df7
 
 ##----anova.p7----
 library(ggplot2)
 # if (!require("ggplot2")) install.packages("ggplot2")
 p7 <- 
-  ggplot(data = df7, mapping = aes(x = EduGroup, y = Consumption)) + 
+  ggplot(data = df7, mapping = aes(x = Variety, y = Yield)) + 
   geom_point() +
   geom_boxplot() +
-  scale_y_continuous(labels = scales::dollar) +
-  labs(x = "Education Group", y = "Weekly consumption expenditures, $") +
+  labs(x = "Variety", y = "Yield") +
   theme_bw()
 print(p7)
 
 ##----anova.fm7----
-fm7 <- lm(formula = Consumption ~ EduGroup, data = df7)
+fm7 <- lm(formula = Yield ~ Variety, data = df7)
 summary(fm7)
 
 ##----anova.fm7anova----
@@ -185,16 +182,15 @@ anova(fm7)
 
 
 ##----ancova.df8----
-Consumption <- c(51, 52, 53, 54, 56, 57, 55, 56, 58, 59, 62, 63)
-Income      <- rep(x = c(80, 90, 100), each = 2)
-Gender      <- gl(n = 2, k = 6, length = 2*6,
-                  labels = c("Male", "Female"), ordered = FALSE)
-df8         <- data.frame(Consumption, Income, Gender)
+Yield    <- c(51, 52, 53, 54, 56, 57, 55, 56, 58, 59, 62, 63)
+Fert     <- rep(x = c(80, 90, 100), each = 2)
+Variety  <- gl(n = 2, k = 6, length = 2*6, labels = c("V1", "V2"), ordered = FALSE)
+df8         <- data.frame(Yield, Fert, Variety)
 df8
 
 
 ##----ancova.fm8----
-fm8 <- lm(formula = Consumption ~ Income + Gender, data = df8)
+fm8 <- lm(formula = Yield ~ Fert + Variety, data = df8)
 summary(fm8)
 
 ##----ancova.fm8anova----
@@ -205,17 +201,17 @@ anova(fm8)
 library(ggplot2)
 # if (!require("ggplot2")) install.packages("ggplot2")
 p8 <- 
-  ggplot(data = df8, mapping = aes(x= Income, y = Consumption, color = Gender)) + 
+  ggplot(data = df8, mapping = aes(x = Fert, y = Yield, color = Variety)) + 
   geom_point() +
   geom_line(mapping = aes(y = predict(fm8))) +
   scale_y_continuous(labels = scales::dollar) +
-  labs(x = "Gender", y = "Weekly consumption expenditures, $") +
+  labs(x = "Fert", y = "Yield") +
   theme_bw()
 print(p8)
 
 
 ##----ancova.fm9----
-fm9 <- lm(formula = Consumption ~ Income * Gender, data = df8)
+fm9 <- lm(formula = Yield ~ Fert * Variety, data = df8)
 summary(fm9)
 
 ##----ancova.fm9anova----
@@ -226,11 +222,11 @@ anova(fm9)
 library(ggplot2)
 # if (!require("ggplot2")) install.packages("ggplot2")
 p9 <- 
-  ggplot(data = df8, mapping = aes(x= Income, y = Consumption, color = Gender)) + 
+  ggplot(data = df8, mapping = aes(x = Fert, y = Yield, color = Variety)) + 
   geom_point() +
   geom_line(mapping = aes(y = predict(fm9))) +
   scale_y_continuous(labels = scales::dollar) +
-  labs(x = "Gender", y = "Weekly consumption expenditures, $") +
+  labs(x = "Fert", y = "Yield") +
   theme_bw()
 print(p9)
 
@@ -274,3 +270,119 @@ pcor(df11)
 ##----corr.fm12----
 fm12 <- lm(formula = TotShLength ~ LeafArea + LeafMoist, data = df11)
 sqrt(summary(fm12)$r.squared)
+
+##----crd.df13----
+ScabIndex <-
+      c(
+        12, 10,  24, 29, 
+        30, 18,  32, 26, 
+         9,  9,  16,  4,
+        30,  7,  21,  9,
+        16, 10,  18, 18,
+        18, 24,  12, 19,
+        10,  4,   4,  5,
+        17,  7,  16, 17
+      )
+Trt  <- factor(
+          x = rep(x = 1:7, times = c(8, 4, 4, 4, 4, 4, 4))
+        , labels = c("O", "F3", "S3", "F6", "S6", "F9", "S9")
+        )
+
+df13 <- data.frame(Trt, ScabIndex)
+df13
+
+##----crd.p13----
+library(ggplot2)
+# if (!require("ggplot2")) install.packages("ggplot2")
+p13 <- 
+  ggplot(data = df13, mapping = aes(x = Trt, y = ScabIndex)) + 
+  geom_point() +
+  geom_boxplot() +
+  labs(x = "Treatment", y = "Scab Index") +
+  theme_bw()
+print(p13)
+
+##----crd.fm13----
+fm13 <- lm(formula = ScabIndex ~ Trt, data = df13)
+summary(fm13)
+
+##----crd.fm13anova----
+anova(fm13)
+
+
+##----rcbd.df14----
+Yield <- c(
+             32.1,	35.6,	41.9,	35.4
+          ,  30.1,	31.5,	37.1,	30.8
+          ,  25.4,	27.4,	33.8,	31.1
+          ,  24.1,	33.0,	35.6,	31.4
+          ,  26.1,	31.0,	33.8,	31.9
+          ,  23.2,	24.8,	26.7,	26.7
+          )
+SoilType <- gl(n = 4, k = 6, length = 4*6
+               , labels = c("I", "II", "III", "IV"), ordered = FALSE)
+Trt      <- gl(n = 6, k = 1, length = 4*6
+               , labels = c("(NH4)2SO4", "NH4NO3", "CO(NH2)2", "Ca(NO3)2", "NaNO3", "Control"), ordered = FALSE)
+
+df14 <- data.frame(Yield, SoilType, Trt)
+df14
+
+##----rcbd.p14----
+library(ggplot2)
+# if (!require("ggplot2")) install.packages("ggplot2")
+p14 <- 
+  ggplot(data = df14, mapping = aes(x = Trt, y = Yield)) + 
+  geom_point() +
+  geom_boxplot() +
+  labs(x = "Treatment", y = "Yield") +
+  theme_bw()
+print(p14)
+
+##----rcbd.fm14----
+fm14 <- lm(formula = Yield ~ SoilType + Trt, data = df14)
+summary(fm14)
+
+##----rcbd.fm14anova----
+anova(fm14)
+
+
+
+##----latin.df15----
+Yield <- c(
+           257, 230, 279, 287, 202,
+           245, 283, 245, 280, 260,
+           182, 252, 280, 246, 250,
+           203, 204, 227, 193, 259,
+           231, 271, 266, 334, 338
+           )
+Row <- factor(x = rep(x = 1:5, each = 5))
+Col <- factor(x = rep(x = 1:5, times = 5))
+Trt <- c(
+          "B", "E", "A", "C", "D"
+        , "D", "A", "E", "B", "C"
+        , "E", "B", "C", "D", "A"
+        , "A", "C", "D", "E", "B"
+        , "C", "D", "B", "A", "E"
+         )
+
+df15 <- data.frame(Yield, Row, Col, Trt)
+df15
+
+##----latin.p15----
+library(ggplot2)
+# if (!require("ggplot2")) install.packages("ggplot2")
+p15 <- 
+  ggplot(data = df15, mapping = aes(x = Trt, y = Yield)) + 
+  geom_point() +
+  geom_boxplot() +
+  labs(x = "Treatment", y = "Yield") +
+  theme_bw()
+print(p15)
+
+##----latin.fm15----
+fm15 <- lm(formula = Yield ~ Row + Col + Trt, data = df15)
+summary(fm15)
+
+##----latin.fm15anova----
+anova(fm15)
+
